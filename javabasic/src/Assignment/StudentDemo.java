9b62a672fb6d67f27a87600031ef9f597baccd9f
Lenovo <Lenovo@DESKTOP-GVHJEFI>
2024-07-08 11:53:52 +0530

    This is my Assignement Project

---
+++ javabasic/src/Assignment/StudentDemo.java
@@ -0,0 +1,33 @@
+package Assignment;
+import java.util.Scanner;
+
+import Day3.Student;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String name;
+		int rollno;
+		char grade;
+		double Percentage;
+		
+		System.out.println("Enter the rollno");
+		rollno=sc.nextInt();
+		
+		System.out.println("Enter the name");
+		name=sc.nextLine();
+		
+		System.out.println("Enter the grade");
+		grade=sc.nextChar();
+		
+		System.out.println("Enter the Percentage");
+		Percentage=sc.nextDouble();
+		
+		Student s1= new Student();
+		System.out.println(s1);
+		sc.close();
+        
+	}
+
+}
