39724c777704e0f8b66f83c3d7982eb35989bf20
Lenovo <Lenovo@DESKTOP-GVHJEFI>
2024-07-08 14:39:03 +0530

    this is static project

---
+++ javabasic/src/Assignment/EmployeeDemo.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Employee.companyName");
+		Employee e= new Employee("jk",107);
+		System.out.println(e);
+		
+		e=new Employee("jenni",104);
+		System.out.println(e);
+
+	}
+
+}
