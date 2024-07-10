dc652e505efd315ee4ca466e1cbe00164a359de6
Lenovo <Lenovo@DESKTOP-GVHJEFI>
2024-07-10 12:11:15 +0530

    Example program for exceptional

---
+++ javabasic/src/Exception/Example.java
@@ -0,0 +1,28 @@
+package Exception;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		int a,b,c;
+		a=10;
+		b=0;
+		c=0;
+		
+		try
+		{
+			c=a/b;
+			System.out.println("a="+a+"b="+b+"c="+c);
+	    }
+		catch(ArithmeticException e)
+		{
+			System.out.println("Exception occured..");
+			b=5;
+			c=a/b;
+			System.out.println("a="+a+"b="+b+"c="+c);
+			
+		}
+
+	}
+
+}
